926b4d827a2c6add69243a8a499e6172685a3a41
nombredeusuariolucasponzoni <139734105+Lucasponzoni@users.noreply.github.com>
2023-08-06 18:21:32 -0300

    Test
    Loader

--- js/main.js
+++ js/main.js
@@ -36,3 +36,20 @@ cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
     document.body.classList.remove("menu-open");
 })
+
+document.onreadystatechange = function() {
+  if (document.readyState !== "complete") {
+      document.querySelector("body").style.visibility = "hidden";
+      document.querySelector("#spinner").style.visibility = "visible";
+  } else {
+      setTimeout(() => {
+          document.querySelector("body").style.visibility = "visible";
+          document.querySelector("#spinner").style.visibility = "hidden";
+          document.querySelector("#spinner").style.opacity = "0";
+          document.querySelector("#spinner").style.transform = "translateY(100%)";
+      }, 400);
+      setTimeout(() => {
+          document.querySelector("#spinner").style.display = "none";
+      }, 2000);
+  }
+};
