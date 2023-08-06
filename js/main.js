470aca806eb252541b227173e00190c35bfe7b8f
nombredeusuariolucasponzoni <139734105+Lucasponzoni@users.noreply.github.com>
2023-08-06 19:20:28 -0300

    Eliminacion de Loader
    Se eminina loader de web porque interfiere con animaciones.

--- js/main.js
+++ js/main.js
@@ -36,25 +36,3 @@ cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
     document.body.classList.remove("menu-open");
 })
-
- /* --- Loading --- */
-
- document.onreadystatechange = function () {
-  if (document.readyState !== "complete") {
-    document.querySelector("body").style.visibility = "hidden";
-    document.querySelector("#loader-abr").style.visibility = "visible";
-  } else {
-    setTimeout(() => {
-      document.querySelector("body").classList.add("loaded");
-      document.querySelector("body").style.visibility = "visible";
-      document.querySelector("#loader-abr").style.visibility = "hidden";
-      document.querySelector("#loader-abr").style.opacity = "0";
-      document.querySelector("#loader-abr").style.transform = "translateY(100%)";
-      document.querySelector("body").classList.remove("disable-scroll");
-    }, 400);
-
-    setTimeout(() => {
-      document.querySelector("#loader-abr").style.display = "none";
-    }, 2000);
-  }
-};
